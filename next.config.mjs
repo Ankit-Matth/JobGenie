bf5b8eb9103bf6469f572e4536253eeaabe4e552
Ankit Matth <ankitmatth111@gmail.com>
2025-08-17 20:34:35 +0530

    added puppeter for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Exclude chrome-aws-lambda from server bundle
+      config.externals.push('chrome-aws-lambda');
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
